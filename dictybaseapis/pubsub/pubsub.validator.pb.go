adfbabc78be88a80e6611a8e13b7de6003bc1125
Siddhartha Basu <biosidd@gmail.com>
2018-03-30 00:24:49 +0000

    update on 03-30-2018:00:03:49

--- dictybaseapis/pubsub/pubsub.validator.pb.go
+++ dictybaseapis/pubsub/pubsub.validator.pb.go
@@ -9,6 +9,7 @@ It is generated from these files:
 
 It has these top-level messages:
 	Reply
+	UserReply
 */
 package pubsub
 
@@ -17,6 +18,7 @@ import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
 import _ "google.golang.org/genproto/googleapis/rpc/status"
+import _ "github.com/dictyBase/go-genproto/dictybaseapis/user"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
@@ -24,6 +26,24 @@ var _ = fmt.Errorf
 var _ = math.Inf
 
 func (this *Reply) Validate() error {
+	if this.Status != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Status); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("Status", err)
+		}
+	}
+	return nil
+}
+func (this *UserReply) Validate() error {
+	if this.User != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.User); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("User", err)
+		}
+	}
+	if this.Users != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Users); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("Users", err)
+		}
+	}
 	if this.Status != nil {
 		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Status); err != nil {
 			return github_com_mwitkow_go_proto_validators.FieldError("Status", err)
