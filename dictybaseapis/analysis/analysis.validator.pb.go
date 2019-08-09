7f259075e510b68d037b7d3fceb684613c4dfa43
Siddhartha Basu <biosidd@gmail.com>
2019-08-09 14:32:40 -0500

    update on 08-09-2019:14:08:40

--- dictybaseapis/analysis/analysis.validator.pb.go
+++ dictybaseapis/analysis/analysis.validator.pb.go
@@ -7,9 +7,9 @@ import (
 	fmt "fmt"
 	math "math"
 	proto "github.com/golang/protobuf/proto"
+	_ "github.com/golang/protobuf/ptypes/empty"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	_ "github.com/mwitkow/go-proto-validators"
-	_ "github.com/golang/protobuf/ptypes/empty"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 )
 
@@ -18,6 +18,21 @@ var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
 
+func (this *BlastDbRequest) Validate() error {
+	if this.TaxonId == "" {
+		return github_com_mwitkow_go_proto_validators.FieldError("TaxonId", fmt.Errorf(`value '%v' must not be an empty string`, this.TaxonId))
+	}
+	if this.Name == "" {
+		return github_com_mwitkow_go_proto_validators.FieldError("Name", fmt.Errorf(`value '%v' must not be an empty string`, this.Name))
+	}
+	if this.Title == "" {
+		return github_com_mwitkow_go_proto_validators.FieldError("Title", fmt.Errorf(`value '%v' must not be an empty string`, this.Title))
+	}
+	if this.Seqtype == "" {
+		return github_com_mwitkow_go_proto_validators.FieldError("Seqtype", fmt.Errorf(`value '%v' must not be an empty string`, this.Seqtype))
+	}
+	return nil
+}
 func (this *BlastDbParams) Validate() error {
 	if this.TaxonId == "" {
 		return github_com_mwitkow_go_proto_validators.FieldError("TaxonId", fmt.Errorf(`value '%v' must not be an empty string`, this.TaxonId))
